bad4985af1ced906c0fb6fcf205db1985e64d49e
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-16 17:01:04 +0600

    lesson 18 menu

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.andrclassworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)      //указали toolbar вместо actionBar и все свойства
+        setSupportActionBar(toolbar)                           //actionBar активити передаст к Toolbar
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+//        menu?.add("menu 10")                                       //програмно
+//        menu?.add("menu 20")
+//        menu?.add("menu 30")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {    //этот метод вызывается тогда, когда кликаем на пункты меню
+        when(item.itemId){
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment1() )
+                    .commit()
+            }
+            R.id.menu2 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment2() )
+                    .commit()
+            }
+            R.id.menu3 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment3() )
+                    .commit()
+            }
+            else -> {}
+        }
+        Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
 }
