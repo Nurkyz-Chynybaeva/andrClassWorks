1f69334d12263d317152e710c34308a004b63faa
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-18 20:31:09 +0600

    20th lesson

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.andrclassworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = SimpleAdapter()
+
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        var list = mutableListOf<String>()
+        for(i in 0..29){
+            list.add("ITEM $i")
+        }
+        adapter.setData(list)
+
     }
 }
