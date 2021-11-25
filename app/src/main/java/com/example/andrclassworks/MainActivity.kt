6f0fc81d7d96e75e86556103d4d2987e7023db95
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-25 13:03:37 +0600

    skdlf

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.andrclassworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -12,61 +13,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("TAG", "Berg: onCreate")
 
-        val txt = findViewById<TextView>(R.id.txt_hello)
-        val btn = findViewById<Button>(R.id.btn)
+        var text = findViewById<TextView>(R.id.txt)
+        val button1 = findViewById<Button>(R.id.btn1)
+        val button2 = findViewById<Button>(R.id.btn2)
 
         var count = 0
-        btn.setOnClickListener {
-           count++
-            //count = count + 1
-
-            //count++
-            //count + 1
-            //count = <- (count + 1)
-            txt.text = count.toString()
+        button1.setOnClickListener {
+            ++count
+            text.text = count.toString()
+        }
+        button2.setOnClickListener {
+            if (text.text != "0") {
+                text.text = "0"
+            }
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("key", "value")
+        outState.putString("count", "count")
+
     }
+
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val value = savedInstanceState.getString("key")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.e("TAG", "Berg: onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.e("TAG", "Berg: onResume")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.e("TAG", "Berg: onRestart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.e("Berg","onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.e("TAG", "Berg: onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.e("TAG", "Berg: onDestroy")
-        // ---
+        val count = savedInstanceState.getString("count")
     }
 }
