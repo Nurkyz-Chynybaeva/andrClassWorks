a4f79b525720c8b885b3a6add2185331ccfe1b96
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-26 12:46:14 +0600

    fjek

--- app/src/main/java/com/example/andrclassworks/Fragment1.kt
+++ app/src/main/java/com/example/andrclassworks/Fragment1.kt
@@ -1,7 +1,5 @@
 package com.example.andrclassworks
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,25 +16,32 @@ class Fragment1: Fragment(R.layout.fragment1) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recycler = view.findViewById<RecyclerView>(R.id.recycler)
-        val layoutManager = LinearLayoutManager(this@Fragment1)
-        val adapter = SimpleAdapter {
+//
+//        val recycler = view.findViewById<RecyclerView>(R.id.recycler)
+//        val layoutManager = LinearLayoutManager(this@Fragment1)
+//        val adapter = SimpleAdapter {
+//
+//            requireContext()
+////            val oFragment =
+////            intent.putExtra("text", "item - $it")
+////            startActivity(intent)
+//
+//        }
+//        recycler.layoutManager = layoutManager
+//        recycler.adapter = adapter
+//        recycler.addItemDecoration(DividerItemDecoration(this@Fragment1, RecyclerView.VERTICAL))
+//
+//        val list = mutableListOf<String>()
+//        for (i in 0..20) {
+//            list.add("ITEM -$i")
+//        }
+//        adapter.setData(list)
+//    }
+}
 
-            requireContext()
-//            val oFragment =
-//            intent.putExtra("text", "item - $it")
-//            startActivity(intent)
 
-
-        }
-        recycler.layoutManager = layoutManager
-        recycler.adapter = adapter
-        recycler.addItemDecoration(DividerItemDecoration(this@Fragment1, RecyclerView.VERTICAL))
-
-        val list = mutableListOf<String>()
-        for (i in 0..20) {
-            list.add("ITEM -$i")
-        }
-        adapter.setData(list)
+    fun setText(enteredText: String){
+        
     }
+
 }
