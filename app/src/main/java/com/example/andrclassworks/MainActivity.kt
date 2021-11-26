ccb1b6b580810bd9b9a46735ecb3b5a43bf443e4
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-26 17:37:27 +0600

    added interface

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -1,39 +1,21 @@
 package com.example.andrclassworks
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFragment()
+    }
 
-        val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-
-//        val recycler = findViewById<RecyclerView>(R.id.recycler)
-//        val layoutManager = LinearLayoutManager(this)
-//        val adapter = SimpleAdapter {
-//            Toast.makeText(this, "ITEM -$it", Toast.LENGTH_SHORT).show()
-//           val fragment = supportFragmentManager.beginTransaction()
-//                fragment.add(R.id.container, Fragment1())
-//                fragment.commit()
-//
-////            val intent = Intent(this, MainActivity2::class.java)
-////            intent.putExtra("text", "item - $it")
-////            startActivity(intent)
-//        }
-//
-//        recycler.layoutManager = layoutManager
-//        recycler.adapter = adapter
-//        recycler.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
-//
-//        val list = mutableListOf<String>()
-//        for (i in 0..20) {
-//            list.add("ITEM -$i")
-//        }
-//        adapter.setData(list)
+        private fun initFragment() {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.containerForFragment1, Fragment1())
+                .commit()
         }
+
 }
---
+++ app/src/main/java/com/example/andrclassworks/OnItemClicked.kt
@@ -0,0 +1,5 @@
+package com.example.andrclassworks
+
+interface OnItemClicked {
+    fun onClicked()
+}
