9b7357c746dbdddc2114c2b1779df529e2f0b33d
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-26 11:24:26 +0600

    fjef

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -3,17 +3,15 @@ package com.example.andrclassworks
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
 
 //        val recycler = findViewById<RecyclerView>(R.id.recycler)
 //        val layoutManager = LinearLayoutManager(this)
