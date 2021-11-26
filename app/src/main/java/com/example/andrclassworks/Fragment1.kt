e4e48686fcda951f862443d4c63faff1863e8b1b
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-26 18:33:26 +0600

    fej

--- app/src/main/java/com/example/andrclassworks/Fragment1.kt
+++ app/src/main/java/com/example/andrclassworks/Fragment1.kt
@@ -2,14 +2,8 @@ package com.example.andrclassworks
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class Fragment1: Fragment(R.layout.fragment1){
@@ -26,8 +20,6 @@ class Fragment1: Fragment(R.layout.fragment1){
         super.onViewCreated(view, savedInstanceState)
 
         val recycler = view.findViewById<RecyclerView>(R.id.recycler)
-
-
 
 }
 
