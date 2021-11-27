2f6526fc48fc196a6174d30702c53a50a9a4ffdd
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-28 00:13:51 +0600

    feji

---
+++ app/src/main/java/com/example/andrclassworks/Fragment1.kt
@@ -0,0 +1,13 @@
+package com.example.andrclassworks
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment1: Fragment(R.layout.fragment1){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
