b50a980667c236977821596ce1713eb015737fe5
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-27 15:51:34 +0600

    calculator

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.andrclassworks
 
-import android.icu.lang.UCharacter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatTextView
+import android.util.Log
 import com.example.andrclassworks.databinding.ActivityMainBinding
-import com.google.android.material.appbar.AppBarLayout
-import kotlin.math.exp
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,42 +19,64 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             //numbers
-            one.setOnClickListener { appendonExpresstion("1", true)}
-            two.setOnClickListener { appendonExpresstion("2", true)}
-            three.setOnClickListener { appendonExpresstion("3", true)}
-            four.setOnClickListener { appendonExpresstion("4", true)}
-            five.setOnClickListener { appendonExpresstion("5", true)}
-            six.setOnClickListener { appendonExpresstion("6", true)}
-            seven.setOnClickListener { appendonExpresstion("7", true)}
-            eight.setOnClickListener { appendonExpresstion("8", true)}
-            nine.setOnClickListener { appendonExpresstion("9", true)}
-            zero.setOnClickListener { appendonExpresstion("0", true)}
+            one.setOnClickListener { appendonExpresstion("1", true) }
+            two.setOnClickListener { appendonExpresstion("2", true) }
+            three.setOnClickListener { appendonExpresstion("3", true) }
+            four.setOnClickListener { appendonExpresstion("4", true) }
+            five.setOnClickListener { appendonExpresstion("5", true) }
+            six.setOnClickListener { appendonExpresstion("6", true) }
+            seven.setOnClickListener { appendonExpresstion("7", true) }
+            eight.setOnClickListener { appendonExpresstion("8", true) }
+            nine.setOnClickListener { appendonExpresstion("9", true) }
+            zero.setOnClickListener { appendonExpresstion("0", true) }
             //operators
-            plus.setOnClickListener{appendonExpresstion("+", false)}
-            minus.setOnClickListener{appendonExpresstion("-", false)}
-            multiplication.setOnClickListener{appendonExpresstion("*", false)}
-            division.setOnClickListener{appendonExpresstion("/", false)}
-            firstBracket.setOnClickListener{appendonExpresstion("(", false)}
-            secondBracket.setOnClickListener{appendonExpresstion(")", false)}
+            plus.setOnClickListener { appendonExpresstion("+", false) }
+            minus.setOnClickListener { appendonExpresstion("-", false) }
+            multiplication.setOnClickListener { appendonExpresstion("*", false) }
+            division.setOnClickListener { appendonExpresstion("/", false) }
+            firstBracket.setOnClickListener { appendonExpresstion("(", false) }
+            secondBracket.setOnClickListener { appendonExpresstion(")", false) }
+            point.setOnClickListener { appendonExpresstion(".", false) }
 
-            clean.setOnClickListener{
+            clean.setOnClickListener {
                 expression.text = ""
                 result.text = ""
             }
-            back.setOnClickListener{
+            back.setOnClickListener {
                 val string = expression.text.toString()
-                if (string.isNotEmpty()){
-                    expression.text = string.substring(0, string.length-1)
+                if (string.isNotEmpty()) {
+                    expression.text = string.substring(0, string.length - 1)
                 }
                 result.text = ""
             }
+            equals.setOnClickListener {
+                try {
+                    val expression = ExpressionBuilder(binding.expression.text.toString()).build()
+                    val result = expression.evaluate()
+                    val longResult = result.toLong()
+                    if (result == longResult.toDouble())
+                        binding.result.text = longResult.toString()
+                    else
+                        binding.result.text = result.toString()
 
+                } catch (e: Exception) {
+                    Log.d("Exeption", "msg:" + e.message)
+                    binding.result.text ="error!"
+                }catch (e:ArithmeticException){
+                    Log.e("Exeption :","msg:" + e.message)
+                    binding.result.text = "На 0 делить нельзя !"
+                }
+
+            }
 
 
         }
     }
 
     fun appendonExpresstion(string: String, canClear: Boolean) {
+        if (binding.result.text.isNotEmpty()) {
+            binding.expression.text = ""
+        }
         if (canClear) {
             binding.result.text = ""
             binding.expression.append(string)
