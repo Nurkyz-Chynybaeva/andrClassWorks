f08a7392960ffe3abf5badc2ff8d9cf3a0a616b7
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-27 17:30:03 +0600

    fej

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -15,51 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button1 = findViewById<AppCompatButton>(R.id.btn)
-        val button2 = findViewById<AppCompatButton>(R.id.btn2)
-        val button3 = findViewById<AppCompatButton>(R.id.btn3)
-        val button4 = findViewById<AppCompatButton>(R.id.btn4)
-        val button5 = findViewById<AppCompatButton>(R.id.btn5)
+        val button1 = findViewById<AppCompatButton>(R.id.btn1M1)
+        button1.setOnClickListener {
+         val intent = Intent(Intent.ACTION_VIEW)
+            intent.type = "image/*"
+            startActivity(intent)
 
-        button1.setOnClickListener {goTo1()}
-        button2.setOnClickListener {goToWeb()}
-        button3.setOnClickListener {goToGallery()}
-        button4.setOnClickListener {goToActivityWithAction1()}
-        button5.setOnClickListener {goToActivityWithAction2()}
-
-    }
-
-    private fun goTo1(){
-        val intent = Intent("myAction")
-        startActivity(intent)
-    }
-
-    private fun goToWeb(){
-        val address = Uri.parse("http://google.com")
-        val intent2 = Intent(Intent.ACTION_VIEW, address)
-        if (intent2.resolveActivity(packageManager) != null){
-            startActivity(intent2)
-        }else{
-            Toast.makeText(this, "hgiejgiegji", Toast.LENGTH_SHORT).show()
         }
+
     }
 
-    private fun goToGallery() {
-        val intent3 = Intent(Intent.ACTION_VIEW)
-        intent3.type = "image/*"
-        startActivity(intent3)
-    }
-
-    private fun goToActivityWithAction1(){
-        val intent4 = Intent(ACTION_1)
-        intent4.putExtra("key", "goToActivityWithAction1")
-        startActivity(intent4)
-    }
-    private fun goToActivityWithAction2(){
-        val intent4 = Intent(ACTION_2)
-        intent4.putExtra("key", "goToActivityWithAction2")
-        startActivity(intent4)
-    }
 
 }
 
