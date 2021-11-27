f3161da695372cd7366247ae3ec1f91a690f0bea
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-27 17:38:56 +0600

    fje

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.andrclassworks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<AppCompatTextView>(R.id.btn1M1)
+        button1.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.type = "image/*"
+            startActivity(intent)
+        }
+
     }
 }
