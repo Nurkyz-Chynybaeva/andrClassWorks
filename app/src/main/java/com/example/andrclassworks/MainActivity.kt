1a014c96b74e2ac248e31a6d37586576a472f1a7
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-29 23:32:30 +0600

    lesson11 2

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.andrclassworks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 import com.example.andrclassworks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+       val btn1 = findViewById<AppCompatButton>(R.id.btn1M1)
+        btn1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.type = "image/*"
+            startActivity(intent)
+        }
     }
 }
