a0c02ef780f7b9447d0cdb7e19de83d786d05006
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-29 16:22:30 +0600

    21th lesson

---
+++ app/src/main/java/com/example/andrclassworks/Adapter.kt
@@ -0,0 +1,25 @@
+package com.example.andrclassworks
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+
+}
