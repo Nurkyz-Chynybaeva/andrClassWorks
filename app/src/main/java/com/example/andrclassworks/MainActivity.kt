e209502f84286ac2f487a582d66bb3f4c2b4048d
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-13 11:21:51 +0600

    hw 5

--- app/src/main/java/com/example/andrclassworks/MainActivity.kt
+++ app/src/main/java/com/example/andrclassworks/MainActivity.kt
@@ -1,17 +1,45 @@
 package com.example.andrclassworks
+import java.util.Scanner
+fun main() {
+    val map = hashMapOf(
+        "JK" to 995290502,
+        "Nadia" to 995120500,
+        "Ayima" to 995050920,
+        "Timka" to 555789124,
+        "Alish" to 900000009
+    )
+    println("number of records:")
+    val records = readLine().parseToInt()
+    val iterator = (1..records).iterator()
+    val read = Scanner(System.`in`)
+    iterator.forEach {
+        println("Enter name and number: ")
+        val name = read.next()
+        val phone = read.nextInt()
+        map[name] = phone
+    }
+    do {
+        println(
+            "Enter your name: " +
+                    "\n type 'x' to exit"
+        )
+        val key = readLine()
+        if (key in map.keys) {
+            println("$key = ${map.get(key)}")
+        } else {
+            println("Error!")
+        }
+    } while (key != "x")
+    read.close()
+}
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.appcompat.widget.AppCompatButton
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main)
-
-        val btn1 = findViewById<AppCompatButton>(R.id.btn1M1)
-        val btn2 = findViewById<AppCompatButton>(R.id.btn2M1)
-
+private fun String?.parseToInt(): Int {
+    if (this == null) return 0
+    if (this == "") return 0
+    return try {
+        this.toInt()
+    } catch (e: NumberFormatException) {
+        println("exception= $e")
+        return 0
     }
 }
